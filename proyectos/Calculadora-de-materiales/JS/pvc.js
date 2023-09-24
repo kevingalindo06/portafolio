09e8840b68ad7423f4444e7e5bae820b26139a23
Kevin Peña <keenpega0603@gmail.com>
2023-09-24 11:50:59 -0500

    nuevo cambio html

--- proyectos/Calculadora-de-materiales/JS/pvc.js
+++ proyectos/Calculadora-de-materiales/JS/pvc.js
@@ -1,19 +1,18 @@
 
-let mostrarSeccion = document.getElementById("resultado")
-mostrarSeccion.style.display = "none";
+/*let calculo = document.getElementById("enviar")
+calculo.addEventListener('click', function (event) {
 
 
 
 
 
 
+})
 
 
 
 
-
-
-
+*/
 
 
 
@@ -22,105 +21,306 @@ mostrarSeccion.style.display = "none";
 let calculo = document.getElementById("enviar")
 calculo.addEventListener('click', function (event) {
 
+  let mts_1 =document.getElementById("m_1").value;
+  let mts_2 =document.getElementById("m_2").value;
+
+  mts_1 = parseFloat(mts_1);
+  mts_2 = parseFloat(mts_2);
 
 
 
-    let mts_1 = document.getElementById("m_1").value;
-    let mts_2 = document.getElementById("m_2").value;
-
-    mts_1 = parseFloat(mts_1);
-    mts_2 = parseFloat(mts_2);
+  if (mts_1 > 0 && mts_2 > 0) {
 
 
+    let ocultarSeccion = document.getElementById("section_datos")
+    ocultarSeccion.style.display = "none";
 
-    if (mts_1 > 0 && mts_2 > 0) {
+    let mostrarSeccion = document.getElementById("opciones")
+    mostrarSeccion.style.display = "flex";
 
-        calMetros_c = mts_1 * mts_2
-
-
-        perimetro = mts_1 + mts_2 + mts_1 + mts_2
+   
+  }
 
 
 
-        let metros_C = document.getElementById("mc");
+  else {
 
-        metros_C.innerHTML = calMetros_c.toFixed(2);
-
-
-        let ocultarSeccion = document.getElementById("section_datos")
-        ocultarSeccion.style.display = "none";
-
-        mostrarSeccion.style.display = "flex"
+    alert("❌❌❌❌ Error: Debes escribir un valor  en cada una de las casillas para poder calcular tus materiales.");
 
 
+  }
+  opcion1();
+  opcion2();
+  opcion3();
 
-        calcularPVC(calMetros_c, perimetro)
+  function opcion1(){
+    let metros = document.getElementById("mc")
+    let tarjeta_pvc = document.getElementById("l_pvc")
+    let tarjeta_omega = document.getElementById("omegas")
+    let tarjeta_viguetas = document.getElementById("viguetas")
+    let tarjeta_perimetral = document.getElementById("Perimetrales")
+    let tarjeta_Angulos = document.getElementById("angulos")
+    let tarjeta_tornillosP = document.getElementById("tornillo_p")
+    let tarjeta_tornillosG = document.getElementById("tornillo_g")
 
+    let metros_c = mts_1*mts_2;
+    let per = mts_1 + mts_1 + mts_2 + mts_2
+
+    const laminas_sin = 1.785;
+    const omegas_sin = 0.81
+    const viguetas_sin = 0.46
+    const perimetral_sin = per / 5.95
+    const angulos_sin = per / 2.44
+    const tornillos_psin = 11
+    const tornillos_gsin = 8
+
+    let laminas
+    let omegas
+    let viguetas
+    let perimetral
+    let angulos
+    let tornillos_p
+    let tornillos_g
+
+    laminas = metros_c / laminas_sin
+    omegas = metros_c * omegas_sin
+    viguetas = metros_c * viguetas_sin
+    perimetral = perimetral_sin
+    angulos = angulos_sin
+    tornillos_p = metros_c * tornillos_psin
+    tornillos_g = metros_c * tornillos_gsin
+
+    metros.innerHTML = metros_c.toFixed(2)
+    tarjeta_pvc.innerHTML = laminas.toFixed(1)
+    tarjeta_omega.innerHTML = omegas.toFixed(0)
+    tarjeta_viguetas.innerHTML = viguetas.toFixed(0)
+    tarjeta_perimetral.innerHTML = perimetral.toFixed(1)
+    tarjeta_Angulos.innerHTML = angulos.toFixed(0)
+    tarjeta_tornillosP.innerHTML = tornillos_p.toFixed(0)
+    tarjeta_tornillosG.innerHTML = tornillos_g.toFixed(0)
+
+
+
+
+  }
+
+  function opcion2(){
+    let metros = document.getElementById("mc_2")
+    let tarjeta_pvc = document.getElementById("l_pvc_2")
+    let tarjeta_omega = document.getElementById("omegas_2")
+    let tarjeta_viguetas = document.getElementById("viguetas_2")
+    let tarjeta_perimetral = document.getElementById("Perimetrales_2")
+    let tarjeta_Angulos = document.getElementById("angulos_2")
+    let tarjeta_tornillosP = document.getElementById("tornillo_p_2")
+    let tarjeta_tornillosG = document.getElementById("tornillo_g_2")
+
+    let metros_c = mts_1*mts_2;
+    let per = mts_1 + mts_1 + mts_2 + mts_2
+
+    const laminas_sin = 1.785;
+    const omegas_sin = 0.81
+    const viguetas_sin = 0.46
+    const perimetral_sin = per / 5.95
+    const angulos_sin = per / 2.44
+    const tornillos_psin = 11
+    const tornillos_gsin = 8
+
+    let laminas
+    let omegas
+    let viguetas
+    let perimetral
+    let angulos
+    let tornillos_p
+    let tornillos_g
+
+    laminas = metros_c / laminas_sin
+    omegas = metros_c * omegas_sin
+    viguetas = metros_c * viguetas_sin
+    perimetral = perimetral_sin
+    angulos = angulos_sin
+    tornillos_p = metros_c * tornillos_psin
+    tornillos_g = metros_c * tornillos_gsin
+
+    metros.innerHTML = metros_c.toFixed(2)
+    tarjeta_pvc.innerHTML = laminas.toFixed(1)
+    tarjeta_omega.innerHTML = omegas.toFixed(0)
+    tarjeta_viguetas.innerHTML = viguetas.toFixed(0)
+    tarjeta_perimetral.innerHTML = perimetral.toFixed(1)
+    tarjeta_Angulos.innerHTML = angulos.toFixed(0)
+    tarjeta_tornillosP.innerHTML = tornillos_p.toFixed(0)
+    tarjeta_tornillosG.innerHTML = tornillos_g.toFixed(0)
+
+
+
+
+  }
+
+  function opcion3(){
+    let metros = document.getElementById("mc_3")
+    let tarjeta_pvc = document.getElementById("l_pvc_3")
+    let tarjeta_omega = document.getElementById("omegas_3")
+    let tarjeta_viguetas = document.getElementById("viguetas_3")
+    let tarjeta_perimetral = document.getElementById("Perimetrales_3")
+    let tarjeta_Angulos = document.getElementById("angulos_3")
+    let tarjeta_tornillosP = document.getElementById("tornillo_p_3")
+    let tarjeta_tornillosG = document.getElementById("tornillo_g_3")
+
+let l_grande = document.getElementById("l_grande")
+let desperdicio = document.getElementById("desperdicio")
+
+
+    let metros_c = mts_1*mts_2;
+    let per = mts_1 + mts_1 + mts_2 + mts_2
+
+   
+    const omegas_sin = 0.81
+    const viguetas_sin = 0.46
+    const perimetral_sin = per / 5.95
+    const angulos_sin = per / 2.44
+    const tornillos_psin = 11
+    const tornillos_gsin = 8
+
+    let laminas
+    let omegas
+    let viguetas
+    let perimetral
+    let angulos
+    let tornillos_p
+    let tornillos_g
+
+    const anchoLamina = 0.30
+    const largoLamina = 5.95
+
+    if(mts_1 >= mts_2){
+      l_grande.innerHTML = mts_1;
+      }
+    
+    else{
+    
+      l_grande.innerHTML = mts_2;
+    
     }
-
-
-
-    else {
-
-        alert("❌❌❌❌ Error: Debes escribir un valor  en cada una de las casillas para poder calcular tus materiales.");
-
-
-    }
+    
 
     
 
-
-    function calcularPVC(calcularMc, per) {
-
-        let tarjeta_pvc = document.getElementById("l_pvc")
-        let tarjeta_omega = document.getElementById("omegas")
-        let tarjeta_viguetas = document.getElementById("viguetas")
-        let tarjeta_perimetral = document.getElementById("Perimetrales")
-        let tarjeta_tornillosP = document.getElementById("tornillo_p")
-        let tarjeta_tornillosG = document.getElementById("tornillo_g")
-        let tarjeta_Angulos = document.getElementById("angulos")
+    let infoDesperdicio = mts_1 - largoLamina
+    
+    desperdicio.innerHTML = infoDesperdicio
 
 
+    laminas = mts_1 / anchoLamina
+    omegas = metros_c * omegas_sin
+    viguetas = metros_c * viguetas_sin
+    perimetral = perimetral_sin
+    angulos = angulos_sin
+    tornillos_p = metros_c * tornillos_psin
+    tornillos_g = metros_c * tornillos_gsin
 
-        const laminas_sin = 1.785;
-        const omegas_sin = 0.81
-        const viguetas_sin = 0.46
-        const perimetral_sin = per / 5.95
-        const angulos_sin = per / 2.44
-        const tornillos_psin = 11
-        const tornillos_gsin = 8
-
-        let laminas
-        let omegas
-        let viguetas
-        let perimetral
-        let angulos
-        let tornillos_p
-        let tornillos_g
-
-        laminas = calcularMc / laminas_sin
-        omegas = calcularMc * omegas_sin
-        viguetas = calcularMc * viguetas_sin
-        perimetral = perimetral_sin
-        angulos = angulos_sin
-        tornillos_p = calcularMc * tornillos_psin
-        tornillos_g = calcularMc * tornillos_gsin
-
-        tarjeta_pvc.innerHTML = laminas.toFixed(1)
-        tarjeta_omega.innerHTML = omegas.toFixed(0)
-        tarjeta_viguetas.innerHTML = viguetas.toFixed(0)
-        tarjeta_perimetral.innerHTML = perimetral.toFixed(1)
-        tarjeta_Angulos.innerHTML = angulos.toFixed(0)
-        tarjeta_tornillosP.innerHTML = tornillos_p.toFixed(0)
-        tarjeta_tornillosG.innerHTML = tornillos_g.toFixed(0)
+    metros.innerHTML = metros_c.toFixed(2)
+    tarjeta_pvc.innerHTML = laminas.toFixed(1)
+    tarjeta_omega.innerHTML = omegas.toFixed(0)
+    tarjeta_viguetas.innerHTML = viguetas.toFixed(0)
+    tarjeta_perimetral.innerHTML = perimetral.toFixed(1)
+    tarjeta_Angulos.innerHTML = angulos.toFixed(0)
+    tarjeta_tornillosP.innerHTML = tornillos_p.toFixed(0)
+    tarjeta_tornillosG.innerHTML = tornillos_g.toFixed(0)
 
 
 
-    }
 
+  }
 
-    event.preventDefault()
+  event.preventDefault();
+
 })
+
+
+
+
+function showContent() {
+  let resultado1 = document.getElementById("resultado1");
+  let resultado2 = document.getElementById("resultado2");
+  let resultado3 = document.getElementById("resultado3");
+  let opcion1 = document.getElementById("opcion1");
+  let opcion2 = document.getElementById("opcion2");
+  let opcion3 = document.getElementById("opcion3");
+
+  if (opcion1.checked) {
+    resultado1.style.display = 'flex';
+  }
+
+  else {
+    resultado1.style.display = 'none';
+
+  }
+
+  if (opcion2.checked) {
+    resultado2.style.display = 'flex';
+
+  }
+
+  else {
+
+    resultado2.style.display = 'none';
+
+  }
+
+  if (opcion3.checked) {
+    resultado3.style.display = 'flex';
+
+  }
+
+  else {
+
+    resultado3.style.display = 'none';
+  }
+}
+
+
+/*
+  function calcularPVC(tarjeta_pvc,tarjeta_omega,tarjeta_viguetas,tarjeta_perimetral,tarjeta_tornillosP,tarjeta_tornillosG,tarjeta_Angulos) {
+
+   
+    const laminas_sin = 1.785;
+    const omegas_sin = 0.81
+    const viguetas_sin = 0.46
+    //const perimetral_sin = per / 5.95
+    const angulos_sin = per / 2.44
+    const tornillos_psin = 11
+    const tornillos_gsin = 8
+
+    let laminas
+    let omegas
+    let viguetas
+    let perimetral
+    let angulos
+    let tornillos_p
+    let tornillos_g
+
+    laminas = calcularMc / laminas_sin
+    omegas = calcularMc * omegas_sin
+    viguetas = calcularMc * viguetas_sin
+    perimetral = perimetral_sin
+    angulos = angulos_sin
+    tornillos_p = calcularMc * tornillos_psin
+    tornillos_g = calcularMc * tornillos_gsin
+
+    tarjeta_pvc.innerHTML = laminas.toFixed(1)
+    tarjeta_omega.innerHTML = omegas.toFixed(0)
+    tarjeta_viguetas.innerHTML = viguetas.toFixed(0)
+    tarjeta_perimetral.innerHTML = perimetral.toFixed(1)
+    tarjeta_Angulos.innerHTML = angulos.toFixed(0)
+    tarjeta_tornillosP.innerHTML = tornillos_p.toFixed(0)
+    tarjeta_tornillosG.innerHTML = tornillos_g.toFixed(0)
+
+
+
+  }
+
+
+ 
+ 
 
 
 
